c445801ba53a9d4c6c2695c9dce95ca01903a895
Luis Lavena <luislavena@gmail.com>
2010-01-11 02:02:52 -0300

    Moved 1.9.1 to patchlevel 378.

--- config/ruby_installer.rb
+++ config/ruby_installer.rb
@@ -78,7 +78,7 @@ module RubyInstaller
     )
 
     Ruby19 = OpenStruct.new(
-      :version => "1.9.1-p243",
+      :version => "1.9.1-p378",
       :url => "http://ftp.ruby-lang.org/pub/ruby/1.9",
       :checkout => 'http://svn.ruby-lang.org/repos/ruby/branches/ruby_1_9_1',
       :checkout_target => 'downloads/ruby_1_9',
@@ -90,7 +90,7 @@ module RubyInstaller
         '--disable-install-doc'
       ],
       :files => [
-        'ruby-1.9.1-p243.tar.bz2'
+        'ruby-1.9.1-p378.tar.bz2'
       ],
       :dependencies => [
         'zlib1.dll',
