6b384a3b64d8b1cc2110daba5b5eeaaacac1981e
Luis Lavena <luislavena@gmail.com>
2010-07-22 15:03:38 -0300

    Added diffutils
    This is required for successful compilation of Ruby.

--- config/devkit.rb
+++ config/devkit.rb
@@ -82,7 +82,8 @@ module DevKitInstaller
         'sed-4.2.1-2-msys-1.0.13-bin.tar.lzma',
         'gawk-3.1.7-2-msys-1.0.13-bin.tar.lzma',
         'grep-2.5.4-2-msys-1.0.13-bin.tar.lzma',
-        'less-436-2-msys-1.0.13-bin.tar.lzma'
+        'less-436-2-msys-1.0.13-bin.tar.lzma',
+        'diffutils-2.8.7.20071206cvs-3-msys-1.0.13-bin.tar.lzma'
       ],
     }
   )
