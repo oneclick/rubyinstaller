976e0adee61df7cfee0449e30a4c616ae93d44b6
Jon <jon.forums@gmail.com>
2010-07-24 23:13:51 -0400

    Allow default build configuration to be overridden.
    Provide an override/build_config.rb file in the root of the project to
    override the default build configuration contained in the files in the
    config/ directory.

--- rakefile.rb
+++ rakefile.rb
@@ -18,6 +18,13 @@ require 'config/ruby_installer'
 # DevKit configuration data
 require 'config/devkit'
 
+# Allow build configuration overrides if override/build_config.rb file
+# exists in the RubyInstaller project root directory
+begin
+  require 'override/build_config'
+rescue LoadError
+end
+
 Dir.glob("#{RubyInstaller::ROOT}/recipes/**/*.rake").sort.each do |ext|
   puts "Loading #{File.basename(ext)}" if Rake.application.options.trace
   load ext
