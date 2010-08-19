3fb9ba951c52b9b4311345fdf5b99c776bd404f2
Jon <jon.forums@gmail.com>
2010-08-19 15:17:54 -0400

    Teach devkitvars to announce itself.

--- resources/devkit/devkitvars.bat
+++ resources/devkit/devkitvars.bat
@@ -2,5 +2,6 @@
 :: manually configuring a Command Prompt environment to use the
 :: DevKit for compiling native Ruby extensions
 @ECHO OFF
+ECHO Adding the DevKit to PATH...
 SET DEVKIT=%~dp0
 SET PATH=%DEVKIT%bin;%DEVKIT%mingw\bin;%PATH%
