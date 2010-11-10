30046b51cec48569adcb9572c0047700a5c27213
Vít Ondruch <v.ondruch@gmail.com>
2010-11-10 17:25:08 +0100

    Disable SSL certificate checking for compatibility with ruby 1.9

--- rake/contrib/uri_ext.rb
+++ rake/contrib/uri_ext.rb
@@ -284,7 +284,10 @@ module URI
       else
         http = Net::HTTP.new(host, port)
       end
-      http.use_ssl = true if self.instance_of? URI::HTTPS
+      if self.instance_of? URI::HTTPS
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
       yield http
     end
   end  
