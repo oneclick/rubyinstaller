0cd4f53d19b554c7dba0c05fe089021c9a8d8c99
Luis Lavena <luislavena@gmail.com>
2010-12-25 19:01:48 -0300

    Upgrade Ruby 1.9.2 to patchlevel 136
    make check results:
    https://gist.github.com/755065

--- config/ruby_installer.rb
+++ config/ruby_installer.rb
@@ -50,7 +50,7 @@ module RubyInstaller
     end
 
     Ruby19 = OpenStruct.new(
-      :version => "1.9.2-p0",
+      :version => "1.9.2-p136",
       :url => "http://ftp.ruby-lang.org/pub/ruby/1.9",
       :checkout => 'http://svn.ruby-lang.org/repos/ruby/branches/ruby_1_9_2',
       :checkout_target => 'downloads/ruby_1_9',
@@ -62,7 +62,7 @@ module RubyInstaller
         '--disable-install-doc'
       ],
       :files => [
-        'ruby-1.9.2-p0.tar.bz2'
+        'ruby-1.9.2-p136.tar.bz2'
       ],
       :dependencies => [
         :ffi, :gdbm, :iconv, :openssl, :pdcurses, :yaml, :zlib
