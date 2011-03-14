f096d45de989ce075be0b92f381feadf6ccc5573
Jon <jon.forums@gmail.com>
2011-03-14 16:23:41 -0400

    Update DevKit tdm-32-4.5.1 artifacts.

--- config/compilers/tdm_mingw.rb
+++ config/compilers/tdm_mingw.rb
@@ -14,10 +14,10 @@ module DevKitInstaller
           'gcc-4.5.1-tdm-1-c++.tar.lzma',
         ],
         :url_2 => [
-          'binutils-2.20.51-1-mingw32-bin.tar.lzma',
+          'binutils-2.21-3-mingw32-bin.tar.lzma',
           'mingwrt-3.18-mingw32-dev.tar.gz',
           'mingwrt-3.18-mingw32-dll.tar.gz',
-          'w32api-3.15-1-mingw32-dev.tar.lzma',
+          'w32api-3.17-1-mingw32-dev.tar.lzma',
           'autoconf2.1-2.13-4-mingw32-bin.tar.lzma',
           'autoconf2.5-2.67-1-mingw32-bin.tar.lzma',
           'autoconf-9-1-mingw32-bin.tar.lzma',
@@ -25,7 +25,15 @@ module DevKitInstaller
           'automake-4-1-mingw32-bin.tar.lzma',
           'gdb-7.2-1-mingw32-bin.tar.lzma',
           'libexpat-2.0.1-1-mingw32-dll-1.tar.gz',
-          'libtool-2.4-1-mingw32-bin.tar.lzma'
+          'libtool-2.4-1-mingw32-bin.tar.lzma',
+          'bsdtar-2.8.3-1-mingw32-bin.tar.bz2',
+          'bsdcpio-2.8.3-1-mingw32-bin.tar.bz2',
+          'libarchive-2.8.3-1-mingw32-dll-2.tar.bz2',
+          'libbz2-1.0.5-2-mingw32-dll-2.tar.gz',
+          'liblzma-4.999.9beta_20100401-1-mingw32-dll-1.tar.bz2',
+          'libintl-0.17-1-mingw32-dll-8.tar.lzma',
+          'libiconv-1.13.1-1-mingw32-dll-2.tar.lzma',
+          'libz-1.2.3-1-mingw32-dll-1.tar.gz'
         ],
       }
     )
