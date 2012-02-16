36c51d3ec4c9eaaf6b4f53bf02d41aa31e0ab565
Luis Lavena <luislavena@gmail.com>
2012-02-16 13:06:50 -0300

    Upgrade Ruby 1.9.3 to patchlevel 125
    Results of 'make check' here:
    https://gist.github.com/1846080

--- config/ruby_installer.rb
+++ config/ruby_installer.rb
@@ -52,7 +52,7 @@ module RubyInstaller
     end
 
     Ruby19 = OpenStruct.new(
-      :version => "1.9.3-p0",
+      :version => "1.9.3-p125",
       :url => "http://ftp.ruby-lang.org/pub/ruby/1.9",
       :checkout => 'http://svn.ruby-lang.org/repos/ruby/branches/ruby_1_9_3',
       :checkout_target => 'downloads/ruby_1_9',
@@ -65,7 +65,7 @@ module RubyInstaller
         '--disable-install-doc'
       ],
       :files => [
-        'ruby-1.9.3-p0.tar.bz2'
+        'ruby-1.9.3-p125.tar.bz2'
       ],
       :dependencies => [
         :ffi, :gdbm, :iconv, :openssl, :pdcurses, :yaml, :zlib, :tcl, :tk
