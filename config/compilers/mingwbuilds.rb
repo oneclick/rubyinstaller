c7239ad154cf439e61e4914ec5132bacdf658704
Jon <jon.forums@gmail.com>
2012-10-09 22:40:19 -0400

    Update mingwbuilds-32-4.7.2

--- config/compilers/mingwbuilds.rb
+++ config/compilers/mingwbuilds.rb
@@ -12,7 +12,7 @@ module DevKitInstaller
       :relocate => 'sandbox/devkit/mingw/mingw',
       :files => {
         :url_1 => [
-          'i686-mingw-w64-gcc-4.7.2-release-c,c++,fortran-sjlj-rev1.7z'
+          'x32-4.7.2-release-win32-sjlj-rev0.7z'
         ],
         :url_2 => [
           'autoconf2.1-2.13-4-mingw32-bin.tar.lzma',
