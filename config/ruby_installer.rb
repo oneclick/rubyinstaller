4f4ab384b8daafee97c3e04c2298a012a751d04a
Luis Lavena <luislavena@gmail.com>
2013-11-23 18:56:22 -0300

    Upgrade Ruby 1.9.3 to patchlevel 484
    Results of 'make test'
    
    ruby 1.9.3p484 (2013-11-22) [i386-mingw32]
    10595 tests, 1912303 assertions, 0 failures, 0 errors, 96 skips

--- config/ruby_installer.rb
+++ config/ruby_installer.rb
@@ -56,7 +56,7 @@ module RubyInstaller
     Ruby19 = OpenStruct.new(
       :version => "1.9.3-p448",
       :short_version => 'ruby19',
-      :url => "http://ftp.ruby-lang.org/pub/ruby/1.9",
+      :url => "http://cache.ruby-lang.org/pub/ruby/1.9",
       :checkout => 'http://svn.ruby-lang.org/repos/ruby/branches/ruby_1_9_3',
       :checkout_target => 'downloads/ruby_1_9',
       :target => 'sandbox/ruby_1_9',
@@ -71,7 +71,7 @@ module RubyInstaller
         "CPPFLAGS='-DFD_SETSIZE=2048'"
       ],
       :files => [
-        'ruby-1.9.3-p448.tar.bz2'
+        "ruby-1.9.3-p484.tar.bz2"
       ],
       :dependencies => [
         :ffi, :gdbm, :iconv, :openssl, :pdcurses, :yaml, :zlib, :tcl, :tk
