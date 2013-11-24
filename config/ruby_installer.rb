532cfb273e58c05a894aaeebbcbf08205f926aa1
Luis Lavena <luislavena@gmail.com>
2013-11-24 16:24:48 -0300

    Upgrade RubyGems to version 1.8.28

--- config/ruby_installer.rb
+++ config/ruby_installer.rb
@@ -147,8 +147,8 @@ module RubyInstaller
 
     RubyGems = OpenStruct.new(
       :release => 'official',
-      :version => '1.8.24',
-      :url => 'http://rubyforge.org/frs/download.php/76073',
+      :version => '1.8.28',
+      :url => 'http://production.cf.rubygems.org/rubygems',
       :checkout => 'http://github.com/rubygems/rubygems.git',
       :checkout_target => 'downloads/rubygems',
       :target => 'sandbox/rubygems',
@@ -157,7 +157,7 @@ module RubyInstaller
         '--no-rdoc'
       ],
       :files => [
-        'rubygems-1.8.24.tgz'
+        'rubygems-1.8.28.tgz'
       ],
       :hooks => [
         'resources/rubygems/operating_system.rb'
